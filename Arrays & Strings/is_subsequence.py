24f2d1aba69f3df0ac9d9e775b23ed513525a221
alirizaercan <alierc000@pbs.edu.pl>
2024-10-01 09:55:40 +0300

    Array&Strings - Day 1 : The problems are solved.

---
+++ Arrays & Strings/is_subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        c = 0
+        for i in range(len(s)):
+            found = False
+            for j in range(c, len(t)):
+                if (s[i] == t[j]):
+                    c = j + 1
+                    found = True
+                    break
+            if not found:
+                return False
+        return True
