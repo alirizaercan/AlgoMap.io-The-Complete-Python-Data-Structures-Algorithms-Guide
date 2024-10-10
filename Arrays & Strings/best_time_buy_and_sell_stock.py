e154a6741602703603887c3141ac83979a933ace
alirizaercan <alierc000@pbs.edu.pl>
2024-10-10 09:57:07 +0300

    Array&Strings - Day 5 : Problem was solved.

---
+++ Arrays & Strings/best_time_buy_and_sell_stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = []
+        min_price = float(inf)
+        for i in (len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            
+            current_profit = prices[i] - min_price
+            profit.append(current_profit)
+        return max(max(profit), 0)
