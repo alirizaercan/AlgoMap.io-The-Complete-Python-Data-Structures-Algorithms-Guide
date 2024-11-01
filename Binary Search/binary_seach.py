9933a66c5998218c5446eacac4c841dde42436bc
alirizaercan <alierc000@pbs.edu.pl>
2024-11-01 10:00:05 +0300

    Binary Search - Day 4 : Binary Search problem solution was fixed.

--- Binary Search/binary_seach.py
+++ Binary Search/binary_seach.py
@@ -3,7 +3,7 @@ class Solution:
         l = 0
         r = len(nums)-1
         
-        while l<r:
+        while l<= r:
             m = (l+r) // 2
             
             if nums[m] == target:
