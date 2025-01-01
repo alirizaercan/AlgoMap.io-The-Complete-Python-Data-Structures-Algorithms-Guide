eb6cc0c158eb10c45f4ea633e5fdd8ebc41a9459
alirizaercan <alierc000@pbs.edu.pl>
2025-01-01 11:58:08 +0300

    Array&Strings - Day 7 : Problem was solved.

---
+++ Arrays & Strings/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        j = 1
+        for i in range(1, n):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+        
+        return j
+    
+    # Time Complexity : O(n)
+    # Space Complexity : O(1)
