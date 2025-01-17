15007413a85d962d22a183a56284694b4c010052
alirizaercan <alierc000@pbs.edu.pl>
2025-01-17 11:37:35 +0300

    Bit Manipulation - Day 3 : Problems were solved. All problems were solved. The roadmap was completed.

---
+++ Bit Manipulation/add_binary.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a, b = int(a, 2), int(b, 2)
+
+        while b:
+            without_carry = a ^ b
+            carry = (a & b) << 1
+            a, b = without_carry, carry
+        
+        return bin(a)[2:]
+    
+    # Time Complexity : O(A+B)
+    # Space Complexity : O(1)
